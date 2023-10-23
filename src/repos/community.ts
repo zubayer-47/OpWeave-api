b541ac2bed6306e759b0c6ba0c32993a5cf70043
A B M Zubayer <zubayerjs.dev@gmail.com>
2023-10-23 23:01:31 +0600

    modify community repo

--- src/repos/community.ts
+++ src/repos/community.ts
@@ -1,9 +1,17 @@
 import prismadb from 'src/libs/prismadb'
 
-export const checkIsCommunityExist = (name: string) =>
+export const checkIsCommunityExistByName = (name: string) =>
   prismadb.community.findFirst({
     where: { name },
     select: {
       name: true
+    }
+  })
+
+export const checkIsCommunityExistById = (community_id: string) =>
+  prismadb.community.findFirst({
+    where: { community_id },
+    select: {
+      community_id: true
     }
   })
