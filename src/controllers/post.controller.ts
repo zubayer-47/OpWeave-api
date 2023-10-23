1beb141a9cf93b1bfd0ecabc2a73c58faff13901
A B M Zubayer <zubayerjs.dev@gmail.com>
2023-10-23 23:00:41 +0600

    post controller modified

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
+import prismadb from 'src/libs/prismadb'
+import { checkMemberIsExist } from 'src/repos/member'
 import BaseController from './base.controller'
 
 class PostController extends BaseController {
@@ -7,7 +9,43 @@ class PostController extends BaseController {
     this.configureRoutes()
   }
 
-  private _createPost = async (_req: Request, _res: Response, _next: NextFunction) => {}
+  private _createPost = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const errors: { [index: string]: string } = {}
+      const c_id = req.originalUrl.replace(/^(\/v1\/c\/)|(\/p\/new)$/g, '')
+      const { title, body } = req.body
+
+      // 1st layer
+      if (!title) errors.title = 'title is required!'
+      if (!body) errors.body = 'body is required!'
+
+      // 2nd layer
+      if (!errors.title && title.length < 3) errors.title = 'title should contains 3 characters at least'
+
+      const member = await checkMemberIsExist(req.user)
+      if (!member) errors.member = `You're not a member of this community`
+
+      if (!Object.keys(errors).length) {
+        console.log({ member: member.member_id, c_id })
+
+        const post = await prismadb.post.create({
+          data: {
+            community_id: c_id,
+            member_id: member.member_id,
+            title,
+            body
+          },
+          select: { post_id: true }
+        })
+
+        res.json({ post_id: post.post_id, title, body }).end()
+      } else {
+        res.status(400).json(errors).end()
+      }
+    } catch (error) {
+      next(error)
+    }
+  }
 
   private _getPost = async (_req: Request, _res: Response, _next: NextFunction) => {}
 
