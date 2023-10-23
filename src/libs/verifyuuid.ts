772011e6b3905d89ebfa66868434b99db0f96fa8
A B M Zubayer <zubayerjs.dev@gmail.com>
2023-10-23 23:05:15 +0600

    added verifyuuid lib to verify uuids

---
+++ src/libs/verifyuuid.ts
@@ -0,0 +1,5 @@
+export const isValidUUId = (uuid: string) => {
+  //   uuid.match(/^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
+
+  return uuid.split('-').length === 5
+}
