a4a5908569f144d79a75aa38c5b1f5766e88d463
A B M Zubayer <zubayerjs.dev@gmail.com>
2023-10-26 19:36:33 +0600

    modified related to getCommunityPosts

--- src/repos/community.ts
+++ src/repos/community.ts
@@ -15,3 +15,39 @@ export const checkIsCommunityExistById = (community_id: string) =>
       community_id: true
     }
   })
+
+export const getPaginatedCommunityPosts = (community_id: string, page: number, limit: number) =>
+  prismadb.post.findMany({
+    where: {
+      community_id
+    },
+    select: {
+      post_id: true,
+      community_id: true,
+      member_id: true,
+      title: true,
+      body: true,
+      createdAt: true,
+      updatedAt: true
+    },
+    orderBy: { createdAt: 'asc' },
+    skip: (page - 1) * limit,
+    take: limit
+  })
+
+export const getCommunityPosts = (community_id: string) =>
+  prismadb.post.findMany({
+    where: {
+      community_id
+    },
+    select: {
+      post_id: true,
+      community_id: true,
+      member_id: true,
+      title: true,
+      body: true,
+      createdAt: true,
+      updatedAt: true
+    },
+    orderBy: { createdAt: 'asc' }
+  })
