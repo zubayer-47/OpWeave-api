299a7f81fd518fbd8ca2c0f83df937423e1933f7
A B M Zubayer <zubayerjs.dev@gmail.com>
2023-10-26 19:37:46 +0600

    worked with getPostByPostId

--- src/repos/post.ts
+++ src/repos/post.ts
@@ -1,1 +1,18 @@
+import prismadb from 'src/libs/prismadb'
 
+export const getPostByPostId = async (community_id: string, post_id: string) =>
+  prismadb.post.findFirst({
+    where: {
+      AND: [{ community_id }, { post_id }]
+    },
+    select: {
+      post_id: true,
+      community_id: true,
+      member_id: true,
+      title: true,
+      body: true,
+      hasPublished: true,
+      createdAt: true,
+      updatedAt: true
+    }
+  })
