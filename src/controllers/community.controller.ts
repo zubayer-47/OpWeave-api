1bfbab8fa8013c7cdeebfbda9a057baf3e434a96
A B M Zubayer <zubayerjs.dev@gmail.com>
2023-11-09 14:18:29 +0600

    work with community related community leave and joining

--- src/controllers/community.controller.ts
+++ src/controllers/community.controller.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from 'express'
 import prismadb from 'src/libs/prismadb'
-import { isValidUUId } from 'src/libs/verifyuuid'
 import {
   checkIsCommunityExistById,
   checkIsCommunityExistByName,
@@ -18,98 +17,99 @@ class CommunityController extends BaseController {
   }
 
   private _createCommunity = async (req: Request, res: Response, next: NextFunction) => {
+    const errors: { [index: string]: string } = {}
+
+    const { name, bio, rules } = req.body
+
+    if (!name) errors.name = 'name is required'
+    if (!bio) errors.bio = 'bio is required'
+    if (!rules?.length) errors.rules = 'rules is required'
+
+    // 2nd layer
+    if (!errors?.name && name.length < 3) errors.name = 'name should contains 3 letters at least'
+    else if (!errors.name && name.match(/[;]$/g)) errors.name = "You can't provide semicolon(;)"
+
+    if (!errors?.bio && bio.length < 4) errors.bio = 'bio should contains 4 letters at least'
+    if (!errors?.rules && !Array.isArray(rules)) errors.rules = 'Rules should be an Array of rules'
+
+    // 3rd layer, check db
+    if (!errors.name) {
+      const existCommunity = await checkIsCommunityExistByName(name)
+
+      if (existCommunity) errors.name = `community "${existCommunity.name}" already exist`
+    }
+    // check whether user exist or not
+    if (req.user.userId) {
+      const userExist = await checkUserExist(req.user.userId)
+      if (!userExist) errors.user = 'User is not valid'
+    }
+
+    if (Object.keys(errors).length) {
+      res.status(400).json(errors).end()
+      return
+    }
+
     try {
-      const errors: { [index: string]: string } = {}
-
-      const { name, bio, rules } = req.body
-
-      if (!name) errors.name = 'name is required'
-      if (!bio) errors.bio = 'bio is required'
-      if (!rules?.length) errors.rules = 'rules is required'
-
-      // 2nd layer
-      if (!errors?.name && name.length < 3) errors.name = 'name should contains 3 letters at least'
-      else if (!errors.name && name.match(/[;]$/g)) errors.name = "You can't provide semicolon(;)"
-
-      if (!errors?.bio && bio.length < 4) errors.bio = 'bio should contains 4 letters at least'
-      if (!errors?.rules && !Array.isArray(rules)) errors.rules = 'Rules should be an Array of rules'
-
-      // 3rd layer, check db
-      if (!errors.name) {
-        const existCommunity = await checkIsCommunityExistByName(name)
-
-        if (existCommunity) errors.name = `community "${existCommunity.name}" already exist`
-      }
-      // check whether user exist or not
-      if (req.user.userId) {
-        const userExist = await checkUserExist(req.user.userId)
-        if (!userExist) errors.user = 'User is not valid'
+      const data = {
+        name,
+        bio,
+        rules: rules.toString()
       }
 
-      if (!Object.keys(errors).length) {
-        const data = {
-          name,
-          bio,
-          rules: rules.toString()
+      const community = await prismadb.community.create({
+        data,
+        select: {
+          community_id: true
         }
+      })
 
-        const community = await prismadb.community.create({
-          data,
-          select: {
-            community_id: true
-          }
-        })
+      // auto create member as admin of created community;
+      await prismadb.member.create({
+        data: {
+          userId: req.user.userId,
+          community_id: community.community_id,
+          role: 'ADMIN',
+          scopes: 'ROOT'
+        },
+        select: {
+          member_id: true
+        }
+      })
 
-        // auto create member as admin of created community;
-        await prismadb.member.create({
-          data: {
-            userId: req.user.userId,
-            community_id: community.community_id,
-            role: 'ADMIN',
-            scopes: 'ROOT'
-          },
-          select: {
-            member_id: true
-          }
-        })
-
-        res
-          .status(201)
-          .json({ community_id: community.community_id, ...data })
-          .end()
-      } else {
-        res.status(400).json(errors).end()
-      }
+      res
+        .status(201)
+        .json({ community_id: community.community_id, ...data })
+        .end()
     } catch (error) {
       next(error)
     }
   }
 
   private _getCommunityPosts = async (req: Request, res: Response, next: NextFunction) => {
+    const errors: { [index: string]: string } = {}
+    const { communityId } = req.params
+    const { page, limit } = req.query
+
+    if (!communityId) errors.community_id = 'content missing'
+
+    // check whether community exist or not
+    const community = await checkIsCommunityExistById(communityId)
+    if (!community) errors.community = 'Community does not exist'
+
+    if (Object.keys(errors).length) {
+      res.status(400).json(errors).end()
+      return
+    }
+
     try {
-      const errors: { [index: string]: string } = {}
-      const { cId } = req.params
-      const { page, limit } = req.query
-
-      const isValidId = isValidUUId(cId)
-      if (!isValidId) errors.community_id = 'content missing'
-
-      // check whether community exist or not
-      const community = await checkIsCommunityExistById(cId)
-      if (!community) errors.community = 'Community does not exist'
-
-      if (!Object.keys(errors).length) {
-        let posts
-        if (page && limit) {
-          posts = await getPaginatedCommunityPosts(cId, +page, +limit)
-        } else {
-          posts = await getCommunityPosts(cId)
-        }
-
-        res.status(200).json({ posts }).end()
+      let posts
+      if (page && limit) {
+        posts = await getPaginatedCommunityPosts(communityId, +page, +limit)
       } else {
-        res.status(400).json(errors).end()
+        posts = await getCommunityPosts(communityId)
       }
+
+      res.status(200).json({ posts }).end()
     } catch (error) {
       next(error)
     }
@@ -118,44 +118,42 @@ class CommunityController extends BaseController {
   private _getMemberPosts = async (_req: Request, _res: Response, _next: NextFunction) => {}
 
   private _joinMember = async (req: Request, res: Response, next: NextFunction) => {
+    const errors: { [index: string]: string } = {}
+    const communityId = req.params?.communityId
+
+    if (!communityId) errors.message = 'Content missing'
+
+    // check whether user exist or not
+    const user = await checkUserExist(req.user.userId)
+    if (!user) errors.user = 'User does not exist'
+
+    // check whether community exist or not where user wants to add
+    const community = await checkIsCommunityExistById(communityId)
+    if (!community) errors.community = 'Community does not exist'
+
+    // check whether member exist or not already
+    const member = await checkMemberIsExist(req.user.userId, communityId)
+    if (member) errors.member = 'Member Already Exist'
+
+    if (Object.keys(errors).length) {
+      res.status(400).json(errors).end()
+      return
+    }
+
     try {
-      const errors: { [index: string]: string } = {}
-      const { community_id } = req.body
+      const joinedMember = await prismadb.member.create({
+        data: {
+          userId: req.user.userId,
+          community_id: communityId
+        },
+        select: {
+          community_id: true,
+          member_id: true,
+          role: true
+        }
+      })
 
-      if (!community_id || (community_id && !isValidUUId(community_id)))
-        errors.error = 'Something is wrong. Please try again'
-
-      // check whether user exist or not
-      const user = await checkUserExist(req.user.userId)
-      if (!user) errors.user = 'User not exist'
-
-      // check whether community exist or not where user wants to add
-      const community = await checkIsCommunityExistById(community_id)
-      if (!community) errors.community = 'Community does not exist'
-
-      // check whether member exist or not already
-      const isMemberExist = await checkMemberIsExist(req.user.userId, community_id)
-      if (isMemberExist?.member_id) errors.member = 'Member Already Exist'
-
-      if (!Object.keys(errors).length) {
-        const createdMember = await prismadb.member.create({
-          data: {
-            userId: req.user.userId,
-            community_id: community_id
-          },
-          select: {
-            member_id: true,
-            community_id: true
-          }
-        })
-
-        res
-          .status(200)
-          .json({ message: 'Member Created Successfully', member: { ...createdMember } })
-          .end()
-      } else {
-        res.status(400).json(errors).end()
-      }
+      res.status(200).json({ message: 'Member Created Successfully', joinedMember }).end()
     } catch (error) {
       next(error)
     }
@@ -185,15 +183,89 @@ class CommunityController extends BaseController {
     }
   }
 
+  private _leaveMember = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const communityId = req.params?.communityId
+    const userId = req.user?.userId
+    const userRole = req.user?.role
+
+    /**
+     * Validation
+     */
+    const errors: { [index: string]: string } = {}
+
+    console.log({ userRole })
+    if (userRole === 'ADMIN') {
+      res.status(400).json('You are Admin of this community')
+      return
+    }
+
+    // check whether community exist or not
+    const community = await checkIsCommunityExistById(communityId)
+    if (!community) {
+      res.status(404).json('Community not found')
+      return
+    }
+    // check whether member exist or not
+    const member = await prismadb.member.findFirst({
+      where: {
+        userId,
+        community_id: communityId,
+        leavedAt: null
+      },
+      select: {
+        member_id: true,
+        community_id: true,
+        role: true
+      }
+    })
+
+    console.log({ member })
+
+    if (!member) errors.member = 'You are not a member of this community'
+
+    if (Object.keys(errors).length) {
+      res.status(400).json(errors)
+      return
+    }
+
+    try {
+      // update leavedAt property of this member
+      await prismadb.member.update({
+        where: {
+          member_id: member?.member_id
+        },
+        data: {
+          leavedAt: new Date()
+        },
+        select: {
+          member_id: true
+        }
+      })
+
+      res
+        .status(200)
+        .json({
+          message: 'member successfully leaved',
+          member: { community_id: member.community_id, member_id: member.member_id }
+        })
+        .end()
+    } catch (error) {
+      next(error)
+    }
+  }
+
   public configureRoutes(): void {
     this.router.post('/new', this._auth, this._createCommunity)
-    this.router.get('/:cId', this._auth, this._getCommunityPosts)
+    this.router.get('/:communityId', this._auth, this._getCommunityPosts)
     // add member
-    this.router.post('/', this._auth, this._joinMember)
+    this.router.post('/:communityId', this._auth, this._joinMember)
     //   GET: queries: (page,limit)
-    this.router.get('/:cId/:mId', this._auth, this._getMemberPosts)
+    this.router.get('/:communityId/:memberId', this._auth, this._getMemberPosts)
 
-    this.router.delete('/:communityId', this._auth, this._deletePost)
+    // delete post
+    this.router.delete('/:communityId/:postId', this._auth, this._deletePost)
+    // leave
+    this.router.delete('/:communityId/:memberId', this._auth, this._leaveMember)
 
     // this.router.use('/:cId/p/', postController.router)
 
