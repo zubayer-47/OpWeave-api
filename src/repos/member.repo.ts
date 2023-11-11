ba723fd195fc2b370101ace33006f443d3bbdcef
A B M Zubayer <zubayerjs.dev@gmail.com>
2023-11-11 15:35:24 +0600

    memberRepo modified

---
+++ src/repos/member.repo.ts
@@ -0,0 +1,52 @@
+import { Prisma } from '@prisma/client'
+import { DefaultArgs } from '@prisma/client/runtime/library'
+import prismadb from 'src/libs/prismadb'
+
+class MemberRepo {
+  private member: Prisma.memberDelegate<DefaultArgs>
+  constructor() {
+    this.member = prismadb.member
+  }
+
+  /**
+   * check member existence
+   * @param user_id this should be userId
+   * @param community_id this should be communityId
+   * @returns member_id community_id and role
+   */
+  public isExist(user_id: string, community_id: string) {
+    return this.member.findFirst({
+      where: {
+        user_id,
+        community_id,
+        leavedAt: null
+      },
+      select: {
+        member_id: true,
+        community_id: true,
+        role: true
+      }
+    })
+  }
+
+  /**
+   * Get Member
+   * @param community_id this should communityId
+   * @param member_id this should memberId
+   * @returns user_id member_id and role
+   */
+  public get(community_id: string, member_id: string) {
+    return this.member.findFirst({
+      where: {
+        community_id,
+        member_id
+      },
+      select: {
+        user_id: true,
+        member_id: true,
+        role: true
+      }
+    })
+  }
+}
+export default new MemberRepo()
