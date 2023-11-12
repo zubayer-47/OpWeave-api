5d93dbd9646b3a313297d8e7ae286735d506a1ee
A B M Zubayer <zubayerjs.dev@gmail.com>
2023-11-12 23:05:57 +0600

    update

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -17,11 +17,11 @@ class MemberController extends BaseController {
       if (!communityId || !memberId) errors.content = 'Content missing'
 
       if (!Object.keys(errors).length) {
-        let posts
+        let posts: unknown
         if (page && limit) {
           posts = await postRepo.getCommunityPosts(communityId, memberId, +page, +limit)
         } else {
-          posts = await postRepo.getCommunityPosts(communityId, memberId)
+          posts = await postRepo.getCommunityPosts(communityId)
         }
 
         res.status(200).json({ posts }).end()
@@ -33,32 +33,32 @@ class MemberController extends BaseController {
     }
   }
 
-  private _getMembers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const {} = req.params
-    const {} = req.body
-    const {} = req.query
-    /**
-     * Validation
-     */
-    const errors: { [index: string]: string } = {}
-    // here gose your validation rules
-    if (Object.keys(errors).length) {
-      res.status(400).json(errors)
-      return
-    }
-    try {
-      // Your async code gose here...
-    } catch (error) {
-      next(error)
-    }
-  }
+  // private _getMembers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  //   const {} = req.params
+  //   const {} = req.body
+  //   const {} = req.query
+  //   /**
+  //    * Validation
+  //    */
+  //   const errors: { [index: string]: string } = {}
+  //   // here gose your validation rules
+  //   if (Object.keys(errors).length) {
+  //     res.status(400).json(errors)
+  //     return
+  //   }
+  //   try {
+  //     // Your async code gose here...
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
 
   public configureRoutes(): void {
     this.router.get('/:communityId/:memberId', this._auth, this._getCommunityPostsByMember)
 
     // make them
     // get community members
-    this.router.get('/:communityId', this._auth, this._getMembers)
+    // this.router.get('/:communityId', this._auth, this._getMembers)
   }
 }
 
