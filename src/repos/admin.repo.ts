adf3221b59caae3a95b263a5b651cd4ee3a0b47c
A B M Zubayer <zubayerjs.dev@gmail.com>
2023-12-25 22:01:52 +0600

    add: admin repo added to handle admin/moderator's data

---
+++ src/repos/admin.repo.ts
@@ -0,0 +1,74 @@
+import { Prisma } from '@prisma/client'
+import { DefaultArgs } from '@prisma/client/runtime/library'
+import prismadb from 'src/libs/prismadb'
+
+class AdminRepo {
+  private member: Prisma.memberDelegate<DefaultArgs>
+  private post: Prisma.postDelegate<DefaultArgs>
+
+  constructor() {
+    this.member = prismadb.member
+    this.post = prismadb.post
+  }
+
+  /**
+   *
+   * @param community_id community ID
+   * @param user_id user ID
+   */
+  public isExist(community_id: string, user_id: string) {
+    return this.member.findFirst({
+      where: {
+        community_id,
+        user_id
+      },
+      select: {
+        member_id: true,
+        role: true,
+        scopes: true
+      }
+    })
+  }
+
+  /**
+   *
+   * @param post_id
+   * @returns
+   */
+  public approvePost(post_id: string) {
+    return this.post.update({
+      where: {
+        post_id
+      },
+      data: {
+        hasPublished: true
+      },
+      select: {
+        post_id: true
+      }
+    })
+  }
+
+  /**
+   *
+   * @param post_id
+   * @returns
+   */
+  public getPost(post_id: string) {
+    return this.post.findFirst({
+      where: {
+        post_id
+      },
+      select: {
+        post_id: true,
+        community_id: true,
+        hasPublished: true
+      }
+    })
+  }
+
+  // public gets() {
+  //   return this.community.findMany()
+  // }
+}
+export default new AdminRepo()
