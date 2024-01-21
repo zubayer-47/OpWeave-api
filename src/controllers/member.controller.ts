e0d11749526bba271c2f9c20e03fbc9b6a10d9fa
A B M Zubayer <zubayerjs.dev@gmail.com>
2024-01-22 00:42:14 +0600

    FIX: fix the controller's layout to use as a controller not a router

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,14 +1,137 @@
 import { NextFunction, Request, Response } from 'express'
+import prismadb from 'src/libs/prismadb'
+import communityRepo from 'src/repos/community.repo'
+import memberRepo from 'src/repos/member.repo'
 import postRepo from 'src/repos/post.repo'
-import BaseController from './base.controller'
+import { ErrorType } from 'src/types/custom'
 
-class MemberController extends BaseController {
-  constructor() {
-    super()
-    this.configureRoutes()
+export default class MemberController {
+  static _getMembers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const {} = req.params
+    const {} = req.body
+    const {} = req.query
+    /**
+     * Validation
+     */
+    const errors: ErrorType = {}
+    // here gose your validation rules
+    if (Object.keys(errors).length) {
+      res.status(400).json(errors)
+      return
+    }
+    try {
+      // Your async code gose here...
+    } catch (error) {
+      next(error)
+    }
   }
 
-  private _getCommunityPostsByMember = async (req: Request, res: Response, next: NextFunction) => {
+  static _joinMember = async (req: Request, res: Response, next: NextFunction) => {
+    const errors: { [index: string]: string } = {}
+    const userId = req.user?.userId
+    const communityId = req.params?.communityId
+
+    // check whether community exist or not where user wants to add
+    const community = await communityRepo.isExist(communityId, 'community_id')
+    if (!community) errors.community = 'Community does not exist'
+
+    // check whether member exist or not already
+    const member = await memberRepo.isExistWithLeavedAt(userId, communityId)
+    // console.log({ member })
+    if (member && !member?.leavedAt) errors.member = 'Member Already Exist'
+
+    if (Object.keys(errors).length) {
+      res.status(400).json(errors).end()
+      return
+    }
+
+    try {
+      if (!member) {
+        const joinedMember = await prismadb.member.create({
+          data: {
+            user_id: userId,
+            community_id: communityId
+          },
+          select: {
+            community_id: true,
+            member_id: true,
+            role: true
+          }
+        })
+
+        res.status(201).json({ message: 'Member Joined Successfully', member: joinedMember })
+        return
+      }
+
+      // if member wants to join again after leaving.
+      const joinedMember = await prismadb.member.update({
+        where: {
+          member_id: member.member_id
+        },
+        data: {
+          leavedAt: null,
+          role: 'MEMBER'
+        },
+        select: {
+          community_id: true,
+          member_id: true,
+          role: true
+        }
+      })
+
+      res.status(201).json({ message: 'Member Created Successfully', member: joinedMember })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static _leaveMember = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const userId = req.user?.userId
+    const userRole = req.user?.role
+    const community_id = req.body?.community_id
+
+    // const errors: { [index: string]: string } = {}
+
+    if (userRole === 'ADMIN') {
+      res.status(400).json('You are Admin of this community. you cannot perform leave action')
+      return
+    }
+
+    // get member_id
+    const member = await memberRepo.getMemberRoleInCommunity(userId, community_id)
+
+    // if (Object.keys(errors).length) {
+    //   res.status(400).json(errors)
+    //   return
+    // }
+
+    try {
+      // update leavedAt property of this member
+      await prismadb.member.update({
+        where: {
+          member_id: member?.member_id
+        },
+        data: {
+          leavedAt: new Date()
+        },
+        select: {
+          member_id: true
+        }
+      })
+
+      res
+        .status(200)
+        .json({
+          message: 'member successfully leaved',
+          member: { community_id: community_id, member_id: member.member_id }
+        })
+        .end()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static _getCommunityPostsByMember = async (req: Request, res: Response, next: NextFunction) => {
     const { page, limit } = req.query
     const member_id = req.params?.memberId
 
@@ -35,33 +158,4 @@ class MemberController extends BaseController {
       next(error)
     }
   }
-  // private _getMembers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  //   const {} = req.params
-  //   const {} = req.body
-  //   const {} = req.query
-  //   /**
-  //    * Validation
-  //    */
-  //   const errors: { [index: string]: string } = {}
-  //   // here gose your validation rules
-  //   if (Object.keys(errors).length) {
-  //     res.status(400).json(errors)
-  //     return
-  //   }
-  //   try {
-  //     // Your async code gose here...
-  //   } catch (error) {
-  //     next(error)
-  //   }
-  // }
-
-  public configureRoutes(): void {
-    this.router.get('/:memberId', this._auth, this._checkRoles, this._getCommunityPostsByMember)
-
-    // make them
-    // get community members
-    // this.router.get('/:communityId', this._auth, this._getMembers)
-  }
 }
-
-export default new MemberController()
